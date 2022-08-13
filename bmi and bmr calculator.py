14f1cb9866e27741117d3dc519ac50b6b1f39fbf
ChayeNovak <ChayeNovak@users.noreply.github.com>
2022-08-13 21:22:22 +0100

    Init
    Initalize repository. Include initial base code

---
+++ bmi and bmr calculator.py
@@ -0,0 +1,20 @@
+import sys
+
+print("Please enter either 'Metric' or 'Imperial' to set the calculator's properties.")
+#def main():
+
+Age = int(input("Please Enter your age"))
+Gender = str(input("Please enter whether you are male or female"))
+Weight = float(input("Please enter your weight in Kilograms(Kg): "))
+Height = float(input("Please enter your Height centimeters(cm): "))
+Height /= 100
+BMI = Weight / (Height ** 2)
+print("Your Body Mass Index (BMI) is: " + "{:.2f}".format(BMI))
+
+def metric(weight, height):
+    return (weight *)
+
+
+if
+
+weight = 0.00
